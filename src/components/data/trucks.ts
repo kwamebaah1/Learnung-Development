409e56a816feac5c5c299a6965d88db944e3baa6
Prince Kwame Baah <134564708+kwamebaah1@users.noreply.github.com>
2025-09-02 09:57:01 +0000

    Data

---
+++ src/components/data/trucks.ts
@@ -0,0 +1,45 @@
+export const trucks = [
+    {
+      name: "Jakob Vetrovs",
+      role: "Volvo FL",
+      status: "ON THE WAY",
+      avatar: "/images/user-5.jpg",
+      initials: "JV"
+    },
+    {
+      name: "Zain Vetrovs",
+      role: "Mercedes-Benz Atego",
+      status: "WAITING",
+      avatar: "/images/user-6.jpg",
+      initials: "ZV",
+      hasAlert: true
+    },
+    {
+      name: "Jaylan Rhiel Madsen",
+      role: "Volvo FL",
+      status: "ON THE WAY",
+      avatar: "/images/user-11.jpg",
+      initials: "JM"
+    },
+    {
+      name: "Gustavo Torff",
+      role: "Volvo FH",
+      status: "UNLOADING",
+      avatar: "/images/user-8.jpg",
+      initials: "GT"
+    },
+    {
+      name: "Jaylen Botosh",
+      role: "Man TGL B 190 4x2 BL CH",
+      status: "LOADING",
+      avatar: "/images/user-9.jpg",
+      initials: "JB"
+    },
+    {
+      name: "Marcus Dokidis",
+      role: "Man TGL B 190 4x2 BL CH",
+      status: "ON THE WAY",
+      avatar: "/images/user-10.jpg",
+      initials: "MD"
+    }
+  ];
