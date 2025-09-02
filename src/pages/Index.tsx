f05ad37bacc127b683f8320a5dc2d38e28d5e9f8
Prince Kwame Baah <134564708+kwamebaah1@users.noreply.github.com>
2025-09-02 09:51:01 +0000

    Mobile navigation

--- src/pages/Index.tsx
+++ src/pages/Index.tsx
@@ -29,7 +29,7 @@ const Index = () => {
       </div>
             
       {/* Main Content */}
-      <div className="flex-1 p-3 lg:p-6 overflow-auto w-full lg:ml-80">
+      <div className="flex-1 p-3 lg:p-5 overflow-auto w-full lg:ml-80">
         <div className="max-w-7xl mx-auto space-y-4 lg:space-y-6">
           {/* Vehicle Details */}
           <VehicleDetails />
