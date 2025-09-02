04e3c07e95931b92978a711997b734614a1b9bf3
Prince Kwame Baah <134564708+kwamebaah1@users.noreply.github.com>
2025-09-02 09:57:28 +0000

    Mock data

---
+++ src/components/data/favoriteDrivers.ts
@@ -0,0 +1,31 @@
+export const favoriteDrivers = [
+    {
+      name: "Nolan Dokidis",
+      role: "Mercedes-Benz Sprinter",
+      status: "ON THE WAY",
+      avatar: "/images/user-2.jpg",
+      initials: "ND"
+    },
+    {
+      name: "Ahmad Mango",
+      role: "Volkswagen Transporter",
+      status: "LOADING",
+      avatar: "/images/user-3.jpg",
+      initials: "AM"
+    },
+    {
+      name: "James Lubin",
+      role: "Volkswagen Transporter",
+      status: "ON THE WAY",
+      avatar: "/images/user-1.jpg",
+      initials: "JL"
+    },
+    {
+      name: "Talan Dorwart",
+      role: "Mercedes-Benz Metris",
+      status: "WAITING",
+      avatar: "/images/user-4.jpg",
+      initials: "TD",
+      hasAlert: true
+    }
+  ];
---
+++ src/components/data/vans.ts
@@ -0,0 +1,30 @@
+export const vans = [
+    {
+      name: "Tiana Westervelt",
+      role: "Volkswagen Transporter",
+      status: "LOADING",
+      avatar: "/images/user-7.jpg",
+      initials: "TW"
+    },
+    {
+      name: "Zain Korsgaard",
+      role: "Mercedes-Benz Sprinter",
+      status: "ON THE WAY",
+      avatar: "/images/user-12.jpg",
+      initials: "ZK"
+    },
+    {
+      name: "Wilson Dokidis",
+      role: "Mercedes-Benz Metris",
+      status: "ON THE WAY",
+      avatar: "/images/user-13.jpg",
+      initials: "WD"
+    },
+    {
+      name: "Jaxson Donin",
+      role: "Volkswagen Transporter",
+      status: "ON THE WAY",
+      avatar: "/images/user-14.jpg",
+      initials: "JD"
+    }
+  ];
